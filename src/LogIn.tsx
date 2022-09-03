4a698192d5f70e5313ffde143c65fcf5b56cda44
Ridhvisaikasam <78377976+Ridhvisaikasam@users.noreply.github.com>
2022-09-03 12:25:09 +0530

    removed unused variables

--- src/LogIn.tsx
+++ src/LogIn.tsx
@@ -5,7 +5,7 @@ export default function LogIn(){
 
     async function onSubmitClick(email:string,password:string){
         try{
-           const account=await firebase.auth().signInWithEmailAndPassword(email,password);
+           await firebase.auth().signInWithEmailAndPassword(email,password);
         }
         catch(e:any){
             console.log(e);
